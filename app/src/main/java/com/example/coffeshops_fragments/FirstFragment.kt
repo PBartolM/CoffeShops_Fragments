facac71be30386d9345ead5e87b07030af8f2b8e
alubar804 <alubar804@ieselcaminas.org>
2021-11-03 20:42:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/coffeshops_fragments/FirstFragment.kt
@@ -0,0 +1,62 @@
+package com.example.coffeshops_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.coffeshops_fragments.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val items = ArrayList<Cafe>()
+        items.add(Cafe(R.drawable.images,R.string.titulo,R.string.subtitulo))
+        items.add(Cafe(R.drawable.images1,R.string.titulo1,R.string.subtitulo1))
+        items.add(Cafe(R.drawable.images2,R.string.titulo2,R.string.subtitulo2))
+        items.add(Cafe(R.drawable.images3,R.string.titulo3,R.string.subtitulo3))
+        items.add(Cafe(R.drawable.images4,R.string.titulo4,R.string.subtitulo4))
+        items.add(Cafe(R.drawable.images5,R.string.titulo5,R.string.subtitulo5))
+        items.add(Cafe(R.drawable.images6,R.string.titulo6,R.string.subtitulo6))
+
+        val recView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recView.setHasFixedSize(true)
+        recView.itemAnimator = DefaultItemAnimator()
+        val adaptador = CafeAdapter(items)
+        recView.adapter = adaptador
+
+        recView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        adaptador.onClick = {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+
+        }
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/coffeshops_fragments/cafe.kt
@@ -0,0 +1,61 @@
+package com.example.coffeshops_fragments
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class Cafe(val imagen:Int,val cadena: Int, val cadena2:Int )
+class CafeAdapter(var items: ArrayList<Cafe>) : RecyclerView.Adapter<CafeAdapter.TarjViewHolder>() {
+    lateinit var onClick : (View) -> Unit
+
+    init {
+        this.items = items
+    }
+
+    class TarjViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var laImagen: ImageView
+        private var texto: TextView
+        private var subTexto: TextView
+        private val rb: RatingBar
+        private var puntos:TextView
+
+        init {
+            laImagen=itemView.findViewById(R.id.iv_image)
+            texto = itemView.findViewById(R.id.textView1)
+            subTexto=itemView.findViewById(R.id.textView3)
+            rb= itemView.findViewById(R.id.ratingBar)
+            puntos= itemView.findViewById(R.id.textView2)
+
+        }
+
+        fun bindTarjeta(t: Cafe, onClick: (View) -> Unit) = with(itemView) {
+            laImagen.setImageResource(t.imagen)
+            texto.setText(t.cadena)
+            subTexto.setText(t.cadena2)
+            setOnClickListener { onClick(itemView) }
+            rb.onRatingBarChangeListener = RatingBar.OnRatingBarChangeListener{ ratingBar, rating, fromUser ->
+
+                puntos.text = rb.rating.toString()
+
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): TarjViewHolder {
+        val itemView = LayoutInflater.from(viewGroup.context).inflate(R.layout.cafe, viewGroup, false)
+        return TarjViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(viewHolder: TarjViewHolder, pos: Int) {
+        val item = items.get(pos)
+        viewHolder.bindTarjeta(item, onClick)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
